5d8458bcdcf3daf1b6309c0f56b063d6611220cf
Andrew Butler <abutler911@gmail.com>
2023-07-07 20:12:26 -0600

    added morse code

--- app.js
+++ app.js
@@ -1,3 +1,65 @@
+const MORSE_CODE = {
+    '....': 'H',
+    '.': 'E',
+    '-.--': 'Y',
+    '.---': 'J',
+    '..-': 'U',
+    '-..': 'D',
+    '.-': 'A',
+    '-...': 'B',
+    '-.-.': 'C',
+    '-..': 'D',
+    '..-.': 'F',
+    '--.': 'G',
+    '....': 'H',
+    '..': 'I',
+    '.---': 'J',
+    '-.-': 'K',
+    '.-..': 'L',
+    '--': 'M',
+    '-.': 'N',
+    '---': 'O',
+    '.--.': 'P',
+    '--.-': 'Q',
+    '.-.': 'R',
+    '...': 'S',
+    '-': 'T',
+    '..-': 'U',
+    '...-': 'V',
+    '.--': 'W',
+    '-..-': 'X',
+    '-.--': 'Y',
+    '--..': 'Z',
+    '-----': '0',
+    '.----': '1',
+    '..---': '2',
+    '...--': '3',
+    '....-': '4',
+    '.....': '5',
+    '-....': '6',
+    '--...': '7',
+    '---..': '8',
+    '----.': '9',
+    '.-.-.-': '.',
+    '--..--': ',',
+    '..--..': '?',
+    '.----.': "'",
+    '-.-.--': '!',
+    '-..-.': '/',
+    '-.--.': '(',
+    '-.--.-': ')',
+    '.-...': '&',
+    '---...': ':',
+    '-.-.-.': ';',
+    '-...-': '=',
+    '.-.-.': '+',
+    '-....-': '-',
+    '..--.-': '_',
+    '.-..-.': '"',
+    '...-..-': '$',
+    '.--.-.': '@',
+    '...---...': 'SOS'
+  };
 function toCamelCase(str){
     if(str === '') {
       console.log('Empty string');
@@ -13,3 +75,21 @@ function toCamelCase(str){
     let result = splitString.join('');
     console.log(result);
   }
+
+
+  function squareDigits(num) {
+    return result = Number(String(num).split('').map(digit => Math.pow(Number(digit), 2)).join(''));
+  }
+
+
+  function decodeMorse(morseCode) {
+    return morseCode.trim().split('   ').map(word =>
+      word.split(' ').map(letter => MORSE_CODE[letter]).join('')
+    ).join(' ');
+  }
+  
+  console.log(squareDigits(9119))
+
+  console.log(decodeMorse('.... . -.--   .--- ..- -.. .')); // Outputs: "HEY JUDE"
+
+  
